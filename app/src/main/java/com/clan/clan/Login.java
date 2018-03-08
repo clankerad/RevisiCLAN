1ae2609ae005a857f865f232c89346c7657dc213
clankerad <ferdy79xxz@gmail.com>
2018-03-08 22:23:08 +0800

    My Acc, layout for activity or fragment

--- app/src/main/java/com/clan/clan/Login.java
+++ app/src/main/java/com/clan/clan/Login.java
@@ -1,7 +1,9 @@
 package com.clan.clan;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void buttonClickFunction(View v)
+    {
+        Intent intent = new Intent(getApplicationContext(), Register.class);
+        startActivity(intent);
     }
 }
