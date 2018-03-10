4fed710c03a1e28408fc9b8581340efce71acb0a
clankerad <ferdy79xxz@gmail.com>
2018-03-10 14:19:53 +0800

    10/03 -- added home layouts

--- app/src/main/java/com/clan/clan/ChangeProfile.java
+++ app/src/main/java/com/clan/clan/ChangeProfile.java
@@ -3,11 +3,11 @@ package com.clan.clan;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MyAccount extends AppCompatActivity {
+public class ChangeProfile extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_account);
+        setContentView(R.layout.activity_change_profile);
     }
 }
